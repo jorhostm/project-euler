a76629cb2344b6925f4af0dc35f07f41ea61282e
Jørgen Høstmark <jorgenhostm@gmail.com>
2024-06-12 18:35:31 +0200

    Moved stack allocations inside function

--- src/P31.c
+++ src/P31.c
@@ -3,10 +3,7 @@
 #define N 200
 #define N_COINS 8
 
-int p[] = {1, 2, 5, 10, 20, 50, 100, 200};
-int memo[N+1][N_COINS] = {0};
-
-int f(int n, int k)
+int f(int n, int k, int p[], int memo[N+1][N_COINS])
 {
     if (n < 0 || k < 0)
         return 0;
@@ -18,12 +15,15 @@ int f(int n, int k)
         return n % p[0] == 0;
     
     if (!memo[n][k])
-        memo[n][k] = f(n-p[k], k) + f(n, k-1);
+        memo[n][k] = f(n-p[k], k, p, memo) + f(n, k-1, p, memo);
     
     return memo[n][k];
 }
 
 void problem31(void)
 {
-    printf("Problem 31:\t%10d", f(N, N_COINS-1));
+    int p[] = {1, 2, 5, 10, 20, 50, 100, 200};
+    int memo[N+1][N_COINS] = {0};
+
+    printf("Problem 31:\t%10d", f(N, N_COINS-1, p, memo));
 }
